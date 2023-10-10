5c6f78d3f240fb9ce88a207c865221e634a63b75
carlosLapa <carlosferreiralapa@gmail.com>
2023-10-10 15:14:10 +0100

    Initial commit

---
+++ src/main/java/com/devsuperior/desafio/DesafioTDD.java
@@ -0,0 +1,13 @@
+package com.devsuperior.desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioTDD {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioTDD.class, args);
+	}
+
+}
